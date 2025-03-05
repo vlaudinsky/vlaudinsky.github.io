e1f7d1a81598116fdbe90cc617a6f6c44556f043
Violet L <vlaudinsky@gmail.com>
2025-03-05 14:12:47 -0600

    Update App.js with the layout

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import { ThemeProvider, createTheme, useColorScheme } from '@mui/material/styles';
+import Header from './components/Header/Header';
+import MainContent from './components/MainContent/MainContent';
 
 function App() {
     const theme = createTheme({
@@ -10,7 +12,8 @@ function App() {
 
     return (
         <ThemeProvider theme={theme} default="system">
-            <div>Page</div>
+            <Header/>
+            <MainContent/>
         </ThemeProvider>
     );
 }
