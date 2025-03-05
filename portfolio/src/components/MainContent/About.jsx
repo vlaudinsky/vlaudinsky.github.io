af47dc237384784bc75b909320e888ac40d40cd1
Violet L <vlaudinsky@gmail.com>
2025-03-05 14:15:04 -0600

    Add skeleton layout for About and Contact

---
+++ portfolio/src/components/MainContent/About.jsx
@@ -0,0 +1,29 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import { useColorScheme } from '@mui/material/styles';
+
+function About() {
+    const { mode, setMode } = useColorScheme();
+    if (!mode) {
+        return null;
+    }
+    return (
+        <Box
+            sx={{
+                display: 'flex',
+                width: '100%',
+                alignItems: 'center',
+                justifyContent: 'center',
+                bgcolor: 'background.default',
+                color: 'text.primary',
+                borderRadius: 1,
+                p: 3,
+                minHeight: '56px',
+            }}
+        >
+            Hello! I am Violet.
+        </Box>
+    );
+}
+export default About;
+
