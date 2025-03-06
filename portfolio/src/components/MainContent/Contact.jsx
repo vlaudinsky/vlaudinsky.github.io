1b7278052b0266ce73a490c3beb1752b0df83d26
Violet L <vlaudinsky@gmail.com>
2025-03-06 12:12:22 -0600

    Add contact.jsx contact information

--- portfolio/src/components/MainContent/Contact.jsx
+++ portfolio/src/components/MainContent/Contact.jsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import { useColorScheme } from '@mui/material/styles';
+import { Button } from '@mui/material';
 
-function About() {
-    const { mode, setMode } = useColorScheme();
-    if (!mode) {
-        return null;
+function Contact() {
+
+    function mailto(encoded){
+        return "mailto:"+ atob(encoded);
     }
     return (
         <Box
@@ -21,9 +21,14 @@ function About() {
                 minHeight: '56px',
             }}
         >
-            About
+            Contact
+            <Box>
+                <Button href={mailto("dmxhdWRpbnNreUBnbWFpbC5jb20=")}>Email</Button>
+                <Button href='https://github.com/vlaudinsky'>Github</Button>
+                <Button href='https://www.linkedin.com/in/violet-laudinsky-209ba3195/'>LinkedIn</Button>
+            </Box>
         </Box>
     );
 }
-export default About;
 
+export default Contact;
