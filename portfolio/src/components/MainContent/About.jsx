7ac72a5bcc074dba65ccfc7abbfc803585d834c5
Violet L <vlaudinsky@gmail.com>
2025-03-07 13:11:23 -0600

    Update About.jsx with placeholders

--- portfolio/src/components/MainContent/About.jsx
+++ portfolio/src/components/MainContent/About.jsx
@@ -1,17 +1,12 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import { useColorScheme } from '@mui/material/styles';
-
+import hero from '../../assets/HeroImage.png'
 function About() {
-    const { mode, setMode } = useColorScheme();
-    if (!mode) {
-        return null;
-    }
     return (
-        <Box
+        <Box id="about"
             sx={{
                 display: 'flex',
-                flexDirection:"column",
+                flexDirection:"row",
                 alignItems: 'center',
                 justifyContent: 'center',
                 bgcolor: 'background.default',
@@ -21,7 +16,13 @@ function About() {
                 minHeight: '56px',
             }}
         >
-            Hello! I am Violet.
+            <Box width="50%">
+                Hello! I am Violet. Bacon ipsum dolor amet anim ipsum irure magna beef sirloin elit commodo non jerky chicken ground round ribeye. Cow chicken aute prosciutto meatloaf, aliquip id mollit occaecat. Consectetur hamburger cillum short ribs minim id corned beef mollit flank. Deserunt kevin et, lorem corned beef ut ut alcatra aliquip sunt consectetur.
+                Magna landjaeger minim shank ribeye strip steak hamburger laboris est tempor sed boudin lorem alcatra. Ribeye ad dolor pig excepteur qui nulla eiusmod tail. Tail sint in tongue reprehenderit salami quis pork biltong cillum doner. Pariatur pancetta culpa in consequat, brisket hamburger nulla pastrami mollit pig ipsum exercitation landjaeger in. Ut ad incididunt, shank meatloaf mollit quis aliquip ham excepteur. Qui andouille in chislic biltong laboris cupidatat rump aliqua sunt. Id sirloin shankle dolore tenderloin.
+            </Box>
+            <Box width="50%" display={"flex"} justifyContent={"center"}>
+                <img src={hero}/>
+            </Box>
         </Box>
     );
 }
