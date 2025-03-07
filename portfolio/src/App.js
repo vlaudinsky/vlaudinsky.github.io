6bcb776059cb550ce007d713a9a1353a6056b880
Violet L <vlaudinsky@gmail.com>
2025-03-07 13:12:37 -0600

    update App.js with proper theme switcher

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,20 +2,29 @@ import './App.css';
 import { ThemeProvider, createTheme, useColorScheme } from '@mui/material/styles';
 import Header from './components/Header/Header';
 import MainContent from './components/MainContent/MainContent';
+import { useState } from 'react';
 
 function App() {
+    const [isLightMode, setIsLightMode] = useState(false);
     const theme = createTheme({
         colorSchemes: {
-            dark: true,
-        },
+            dark: isLightMode,
+            light: !isLightMode
+            },
+        palette: {
+            type: 'dark', // Set the theme to dark mode
+            primary: {
+              main: '#e91e63' // Set the primary color to pink
+            },
+        }
     });
-
+    
     return (
-        <ThemeProvider theme={theme} default="system">
-            <div>
-                <Header/>
+        <ThemeProvider theme={theme} default="light">
+                <Header 
+                    isLightMode={isLightMode}
+                    setIsLightMode={setIsLightMode}/>
                 <MainContent/>
-            </div>
         </ThemeProvider>
     );
 }
